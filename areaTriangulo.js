6f30f29924970ddcf951243708634959950d0e6a
mynlop <mynlop15@gmail.com>
2017-05-06 05:36:47 -0600

    1: calcular el area de un triangulo

---
+++ areaTriangulo.js
@@ -0,0 +1,12 @@
+let base = 5
+let height = 7
+
+// function trianguloArea(base, height){
+// 	return base * height / 2
+// }
+
+const trianguloArea = (base, height) => base * height / 2
+
+//const = el contenido de la variable no va a cambiar
+
+console.log(`El área de un triangulo de base ${base} y altura ${height} es: ${trianguloArea(base,height)}`)
