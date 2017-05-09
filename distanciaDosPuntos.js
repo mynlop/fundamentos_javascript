f38abd61f1bacb5b36f60f7d5fc1aeb8933f9c9b
mynlop <mynlop15@gmail.com>
2017-05-08 19:40:45 -0600

    10: crear objetos

--- distanciaDosPuntos.js
+++ distanciaDosPuntos.js
@@ -1,21 +1,43 @@
-function Punto(x,y){
-	this.x = x
-	this.y = y
+// function Punto(x,y){
+// 	this.x = x
+// 	this.y = y
+// }
+
+// Punto.prototype.moverEnX = function (x){
+// 	this.x += x
+// }
+// Punto.prototype.moverEnY = function (y){
+// 	this.y += y
+// }
+// Punto.prototype.distancia = function distancia(p){
+// 	const x = this.x - p.x
+// 	const y = this.y - p.y
+// 	return Math.sqrt(x * x + y * y)
+// }
+const Punto = {
+	init: function(x,y){
+		this.x = x
+		this.y = y
+	},
+	moverEnX: function moverEnX(x){
+		this.x += x
+	},
+	moverEnY: function moverEnY(y){
+		this.y += y
+	},
+	distancia: function distancia(p){
+		const x = this.x - p.x
+		const y = this.y - p.y
+
+		return Math.sqrt(x*x-y*y)
+	}
 }
 
-Punto.prototype.moverEnX = function (x){
-	this.x += x
-}
-Punto.prototype.moverEnY = function (y){
-	this.y += y
-}
-Punto.prototype.distancia = function distancia(p){
-	const x = this.x - p.x
-	const y = this.y - p.y
-	return Math.sqrt(x * x + y * y)
-}
-const p1 = new Punto(0,4); 
-const p2 = new Punto(3,0);
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
+
+p1.init(0,4) 
+p2.init(3,0)
 
 
 console.log(p1.distancia(p2))
