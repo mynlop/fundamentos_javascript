a73ff71e2cb0ac79b37ec4c5e911c973167e6a96
mynlop <mynlop15@gmail.com>
2017-05-08 19:31:52 -0600

    9: prototipos

--- distanciaDosPuntos.js
+++ distanciaDosPuntos.js
@@ -1,22 +1,26 @@
-const p1 = {
-	x: 0,
-	y: 4,
-	moverEnX(x){ this.x += x },
-	moverEnY(y){ this.y += y }
-}
-const p2 = {
-	x: 3,
-	y: 0,
-	moverEnX(x){ this.x += x },
-	moverEnY(y){ this.y += y }
+function Punto(x,y){
+	this.x = x
+	this.y = y
 }
 
-function distancia(p1,p2){
-	const x = p1.x - p2.x
-	const y = p1.y - p2.y
-
+Punto.prototype.moverEnX = function (x){
+	this.x += x
+}
+Punto.prototype.moverEnY = function (y){
+	this.y += y
+}
+Punto.prototype.distancia = function distancia(p){
+	const x = this.x - p.x
+	const y = this.y - p.y
 	return Math.sqrt(x * x + y * y)
 }
+const p1 = new Punto(0,4); 
+const p2 = new Punto(3,0);
 
-console.log(distancia(p1,p2))
-console.log(p1, {x:20,y:-1})
+
+console.log(p1.distancia(p2))
+p1.moverEnX(10)
+console.log(p1.distancia(p2))
+p2.moverEnY(4)
+console.log(p1.distancia(p2))
+// console.log(p1, {x:20,y:-1})
