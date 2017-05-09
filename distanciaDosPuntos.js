5b552022362da23c41e3f68a7a805f2188ccdf52
mynlop <mynlop15@gmail.com>
2017-05-08 22:12:10 -0600

    11: clases

--- distanciaDosPuntos.js
+++ distanciaDosPuntos.js
@@ -1,44 +1,23 @@
-// function Punto(x,y){
-// 	this.x = x
-// 	this.y = y
-// }
-
-// Punto.prototype.moverEnX = function (x){
-// 	this.x += x
-// }
-// Punto.prototype.moverEnY = function (y){
-// 	this.y += y
-// }
-// Punto.prototype.distancia = function distancia(p){
-// 	const x = this.x - p.x
-// 	const y = this.y - p.y
-// 	return Math.sqrt(x * x + y * y)
-// }
-const Punto = {
-	init: function(x,y){
+class Punto{
+	constructor (x,y){
 		this.x = x
 		this.y = y
-	},
-	moverEnX: function moverEnX(x){
+	}
+	moverEnX(x){
 		this.x += x
-	},
-	moverEnY: function moverEnY(y){
+	}
+	moverEnY(y){
 		this.y += y
-	},
-	distancia: function distancia(p){
+	}
+	distancia(p){
 		const x = this.x - p.x
 		const y = this.y - p.y
-
-		return Math.sqrt(x*x-y*y)
+		return Math.sqrt(x * x + y * y)
 	}
 }
 
-const p1 = Object.create(Punto)
-const p2 = Object.create(Punto)
-
-p1.init(0,4) 
-p2.init(3,0)
-
+const p1 = new Punto(0,4)
+const p2 = new Punto(3,0)
 
 console.log(p1.distancia(p2))
 p1.moverEnX(10)
