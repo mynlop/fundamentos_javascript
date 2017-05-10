b7743a8e6df311bd90979ade5ec117e4088e76b4
mynlop <mynlop15@gmail.com>
2017-05-09 19:55:05 -0600

    12: scope de variables

---
+++ scope.js
@@ -0,0 +1,12 @@
+var nombre = "Mynor"
+
+function saludar(nombre){
+    console.log(`Hola ${nombre}`)
+}
+
+saludar("Santiago") //Hola Santiago
+
+//el uso de var ya no es recomendado
+// utilizar: 
+// -> let
+// -> const
