9dd6438256eb4cc54701dd80ead7ce027e0e2f6c
Mynor Lopez <mynlop15@gmail.com>
2017-07-12 18:19:09 -0600

    async-await

---
+++ async-await.js
@@ -0,0 +1,16 @@
+function _handleError(err){
+    console.log(`Request failed: ${err}`)
+}
+
+async function getLuke(){
+  try{
+    const response = await fetch("https://www.swapi.co/api/people/1/")
+    const luke = await response.json()
+    const responseHomeWorld = await fetch(luke.homeworld)
+    luke.homeworld = await responseHomeWorld.json()
+    console.log(`${luke.name} nacio en ${luke.homeworld.name}`)
+  }
+  catch(err){
+    _handleError(err)
+  }
+}
