0888462212e8ad49c09e9ff60a448c12dbfde625
Mynor Lopez <mynlop15@gmail.com>
2017-07-13 21:19:58 -0600

    iteradores

---
+++ iteradores.js
@@ -0,0 +1,32 @@
+function fibonacci(){
+    let a = 0, b = 1
+    return{
+        next: function(){
+            let f = a
+            a = b
+            b = f + a
+            return { value: f, done: false}
+        }
+    }
+}
+
+// const fibo = new fibonacci()
+// fibo.next().value
+// fibo.next().value
+// fibo.next().value
+
+const fibol = {nombre : 'Sacha'}
+fibol[Symbol.iterator] = fibonacci
+
+
+let i = 0
+for(let value of fibol){
+    console.log(value)
+    i++
+    if(i > 20) break
+}
+// los iteradores nos permiten hacer listas infinitas de de elementos
+// para los iteradores podemos obtener lo siguiente
+// next() = itera los datos
+// value = nos devolvera el valor del dato
+// done = sera un indicador para cuando la lista se haya terminado
