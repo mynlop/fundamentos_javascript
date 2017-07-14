a1cbea9ee415039dfb8744edbb08eb0bc818feb3
Mynor Lopez <mynlop15@gmail.com>
2017-07-13 21:43:33 -0600

    inmutabilidad

---
+++ inmutabilidad.js
@@ -0,0 +1,6 @@
+// la inmutabilidad de los objetos es algo importante para la programacion funcional
+// el operador === ayuda a comparar objetos, ejecutando la comparacion no directamente a los datos del objeto sino a la referencia del objeto
+// cuando asignamos un objeto a otro estamos haciendo que ambos apunten a la misma referencia por eso al modificar un objeto el otro tambien se vera afectado porque ambos tienen la misma referencia de memoria.
+// la inmutabilidad tiene por objeto hacer que los parametros de un objeto sean no modificables o inmutables.
+
+// link de referencia: https://facebook.github.io/inmutable-js
