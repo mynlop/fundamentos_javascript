289bb841f90a4fe26365a8eab25c50c3be8f9ccd
Mynor Lopez <mynlop15@gmail.com>
2017-07-13 21:29:15 -0600

    generadores y se agrego codigo a iteradores

---
+++ generadores.js
@@ -0,0 +1,13 @@
+function* fibonacci(){
+    let a = 0, b = 1
+    while(true){
+        let f = a
+        a = b
+        b = f + a
+        let reset = yield f
+        if(reset) a=0, b=1
+    }
+}
+
+const fibo = fibonacci()
+fibo.next()
